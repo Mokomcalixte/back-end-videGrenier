8b7039f56b22d3acb0ce5694723a1471abfa397e
Mokomcalixte <calixtemokom@gmail.com>
2024-03-25 16:17:57 +0100

    signuppage

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log('hello world');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path'); // Importez le module path
+
+const signuoController = require('./controllers/signupController')
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_DB_CONNECT)
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((err) => console.log('Connexion à MongoDB échouée !', err));
+
+const app = express();
+
+// Configuration de bodyParser pour analyser les données JSON et les données de formulaire
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Route GET pour la page d'accueil
+app.get('/', (req, res) => {
+  res.send('<h1>Bienvenue sur notre page d\'accueil!</h1>');
+});
+
+app.post('/signup', signuoController.signup);
+
+
+
+
+// Démarrage du serveur
+app.listen(3005, () => {
+  console.log('Serveur démarré sur le port 3005');
+});
+
+
---
+++ models/signupModel.js
@@ -0,0 +1,29 @@
+//voici mon page signupModel, qu'est ce que je dois dois mettre dans le signuoController
+const User = require ('../schemas/user');
+
+
+// Fonction pour créer un nouvel utilisateur
+const createUser = async (userData) => {
+    try {
+        const newUser = new User(userData);
+        return newUser.save();
+    } catch (error) {
+        throw new Error('Could not create user');
+    }
+};
+
+// Fonction pour rechercher un utilisateur par son adresse e-mail
+const findUserByEmail = async (email) => {
+    try {
+        const user = await User.findOne({ email });
+        return user;
+    } catch (error) {
+        throw new Error('Could not find user by email');
+    }
+};
+
+
+module.exports = {
+    createUser,
+    findUserByEmail
+};
